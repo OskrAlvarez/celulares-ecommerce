15b14eb4b0f09af7d2ca517cf5dfbbe454616400
Oscardev <oscardev@gmail.com>
2025-10-08 19:52:45 -0400

    fix(auth): remove logic of user is signIn to move to AuthGuard

--- src/layouts/ClientLayout.tsx
+++ src/layouts/ClientLayout.tsx
@@ -1,52 +1,37 @@
-import { supabase } from "@/common/supabase/client";
-import { Loader } from "@/components";
-import { singOut, useUser } from "@/features/auth";
-import { useEffect } from "react";
-import { NavLink, Outlet, useNavigate } from "react-router-dom";
+import { AuthGuard, singOut, useUser } from "@/features/auth";
+import { NavLink, Outlet } from "react-router-dom";
 
 export function ClientLayout() {
-
-  const { session, isLoading:isLoadingSession } = useUser()
-  const navigate = useNavigate()
-
-  useEffect(() => {
-    supabase.auth.onAuthStateChange(async (event, session) => {
-      if (event === 'SIGNED_OUT' || !session) {
-        navigate('/login')
-      }
-    })
-  }, [navigate])
-
-  if (isLoadingSession) return <Loader />
+  const { isLoading: isLoadingSession, session } = useUser();
 
   const handleLogOut = async () => {
-    await singOut()
-  }
+    await singOut();
+  };
 
   return (
     <div className="flex flex-col gap-5">
       {/* Menu */}
       <nav className="flex justify-center gap-10 text-sm font-medium">
         <NavLink
-          to='account/orders'
-          className={({ isActive }) => `${isActive ? 'underline' : ''} hover:underline`}
+          to="account/orders"
+          className={({ isActive }) =>
+            `${isActive ? "underline" : ""} hover:underline`
+          }
         >
           Pedidos
         </NavLink>
 
         {/* TODO: LINK DASHBOARD */}
 
-        <button
-          className="hover:underline"
-          onClick={handleLogOut}
-        >
+        <button className="hover:underline" onClick={handleLogOut}>
           Cerrar Sesión
         </button>
       </nav>
-
       <main className="container mt-12 flex-1">
-        <Outlet />
+        <AuthGuard isLoadingSession={isLoadingSession} session={session}>
+          <Outlet />
+        </AuthGuard>
       </main>
     </div>
-  )
+  );
 }
